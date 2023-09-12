164182f7794733ebea9fe0468bd1b575bc6acd0c
darkrazor99 <tarektheman99@gmail.com>
2023-09-12 06:51:07 +0200

    finished index

---
+++ app.js
@@ -0,0 +1,15 @@
+const btns = document.querySelectorAll(".btn");
+const value = document.getElementById("value")
+btns.forEach((btn) => {
+
+    btn.addEventListener("click", (e) => {
+        if (btn.textContent === "DECREASE") {
+            
+
+        } else if (btn.textContent === "RESET") {
+        }
+        else {
+        }
+
+    });
+});
